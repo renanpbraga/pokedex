fb8d4c11603caddf0c3afcba838a2afb0ed5c72a
renanpbraga <renanpbraga@gmail.com>
2021-08-14 00:19:21 -0300

    adiciona funçao de chamada para API, adiciona estados iniciais

--- src/context/PokeProvider.js
+++ src/context/PokeProvider.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import PokeContext from './PokeContext';
 
 function PokeProvider({ children }) {
+  const [data, setData] = useState();
+  const [pkmnID, setPkmnID] = useState(1);
+
+  const  getPokemon = () => {
+    const endpoint = `https://pokeapi.co/api/v2/pokemon/${pkmnID}`;
+    fetch(endpoint)
+      .then((response) => response.json()
+        .then((results) => setData(results)))
+  }
+  
+  useEffect(() => {
+    getPokemon();
+  }, [pkmnID]);
+
+  const context =  {
+    data,
+    setData,
+    pkmnID,
+    setPkmnID,
+  }
+
   return (
     <PokeContext.Provider value={ context }>
       { children }
